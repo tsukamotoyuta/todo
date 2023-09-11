bdbe54ccadd33c283f9745dcb947c4646d003adf
tsukamotoyuta <hayate3751@gmail.com>
2023-09-11 20:21:24 +0900

    first commit

--- todo/src/pages/homePage.js
+++ todo/src/pages/homePage.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
 
 const TestPage = () => {
   return (
     <div>
-      <a href="/test">testへ</a>
+    <FullCalendar
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth" // カレンダーの初期ビューを設定
+      events={[
+        // イベントデータをここに設定
+        { title: "イベント1", date: "2023-09-05" },
+        { title: "イベント2", date: "2023-09-10" },
+        // 他のイベントを追加
+      ]}
+    />
     </div>
   );
 }
